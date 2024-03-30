2bb213feddb56b4cf813daf83eb759ca135e8d5b
Aceed <forjetx@gmail.com>
2024-03-30 18:24:35 +0300

    first commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,20 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    articles: [
+      {
+        title: "Title 1",
+        completed: false
+      },
+      {
+        title: "Title 2",
+        completed: true
+      },
+      {
+        title: "Title 3",
+        completed: false
+      }
+    ]
   },
   getters: {
   },
